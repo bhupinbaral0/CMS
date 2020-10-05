ee1386e57e12aa5c11b32e303ff8cdd9d32d55ab
chandra shekhar neupane <35414590+chandrashekhar07@users.noreply.github.com>
2020-10-05 20:07:17 +0545

    Update settings.py

--- canteen/canteen/settings.py
+++ canteen/canteen/settings.py
@@ -150,4 +150,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'sec073bex@gmail.com'
-EMAIL_HOST_PASSWORD = 'sec073bex@gmail'
+EMAIL_HOST_PASSWORD = 'password'
